cd49fb691dbb55f2061b4f70ac0781a4428b873f
Rajesh Bagguva <rajesh.bagguva@tcs.com>
2024-03-06 09:31:00 +0530

    All pages are created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/layouts/Footer";
+import Navbar from "./components/layouts/Navbar";
+import AboutPage from "./components/pages/AboutPage";
+import HomePage from "./components/pages/HomePage";
+import NotFoundPage from "./components/pages/NotFoundPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/notfound" element={<NotFoundPage />} />
+            <Route path="/*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/pages/AboutPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
